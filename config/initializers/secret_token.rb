16e8882d36d160d837ef91794c724ba5dc9636a2
Fayimora Femi-Balogun <fayimora@hotmail.co.uk>
2012-06-30 02:02:54 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitta::Application.config.secret_token = 'c00e05ed49703f877138b32ebdcf834b4cd2a5d29600b9a41270ef8758848f9d08c8988b76bac4e7052b6d2f00285980651b9c34387b64fed6e5af842ab394e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Twitta::Application.config.session_store :cookie_store, key: '_twitta_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Twitta::Application.config.session_store :active_record_store

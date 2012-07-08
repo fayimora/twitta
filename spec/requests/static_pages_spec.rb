bd2612b87ffb8a62dd722b6497ce492429e399a2
Fayimora Femi-Balogun <fayimora@hotmail.co.uk>
2012-07-08 05:34:37 +0100

    Finished initial layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,47 +1,40 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-
+  subject {page}
   describe "Home Page" do
-    it "should have the content 'Twitta'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text  => 'Twitta')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text  => "Twitta | Home")
-    end
+    before {visit root_path }
+    it { should have_selector('h1', text: 'Twitta') }
+    it { should have_selector('title', text: full_title("Home")) }
   end
   describe "Help Page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text  => 'Help')
-    end
+    before {visit help_path}
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: "Twitta | Help") }
+  end
+  describe "About Page" do
+    before { visit about_path }
+    it { should have_selector('h1', text: 'About us') }
+    it { should have_selector('title', text: "Twitta | About us") }
+  end
+  describe "Contact Page" do
+    before {visit contact_path}
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: "Twitta | Contact") }
+  end
 
-    it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text  => "Twitta | Help")
-    end
-  end
-  describe "About page" do
-    it "should have the content 'About us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text  => 'About us')
-    end
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text  => "Twitta | About us")
-    end
-  end
-  describe "Contact page" do
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text  => 'Contact')
-    end
-    it "should have the right title" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', :text  => "Twitta | Contact")
-    end
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    should have_selector 'title', text: full_title('About us')
+    click_link "Help"
+    should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    should have_selector 'title', text: full_title('Sign up')
+    # click_link "twita"
+    # should have_selector 'h1', text: 'Twitta'
   end
 end

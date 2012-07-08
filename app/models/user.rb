36fdc856f79f5b0ab7774d5b7c18bc1f68279651
Fayimora Femi-Balogun <fayimora@hotmail.co.uk>
2012-07-08 16:41:36 +0100

    Implemented a basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+
+  before_save {|user| user.email=user.email.downcase}
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, format: EMAIL_REGEX,
+                    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}
+  validates :password_confirmation, presence: true
+end
